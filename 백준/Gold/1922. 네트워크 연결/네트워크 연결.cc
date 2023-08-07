c8df25c8ad66d02c6bd1950e84df49602fe87483
Eunjong Kim <eunjong147@pusan.ac.kr>
2023-08-07 18:21:48 +0900

    [Gold IV] Title: 네트워크 연결, Time: 100 ms, Memory: 3200 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int parent[1001];
+
+int find_parent(int a) {
+    if(a == parent[a]) {
+        return a;
+    }
+
+    return parent[a] = find_parent(parent[a]);
+}
+
+void union_parent(int a, int b) {
+    a = find_parent(a);
+    b = find_parent(b);
+
+    parent[b] = a;
+}
+
+bool is_same_parent(int a, int b) {
+    return find_parent(a) == find_parent(b);
+}
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    vector<pair<int, pair<int, int>>> v(M);
+
+    int v1, v2, cost;
+    for(int i=0; i<M; i++) {
+        cin >> v1 >> v2 >> cost;
+
+        v[i] = {cost, {v1, v2}};
+    }
+
+    sort(v.begin(), v.end());
+
+    for(int i=0; i<=N; i++) {
+        parent[i] = i;
+    }
+
+    int total = 0;
+    for(auto a : v) {
+        if(is_same_parent(a.second.first, a.second.second)) {
+            continue;
+        }
+
+        total += a.first;
+        union_parent(a.second.first, a.second.second);
+    }
+
+    cout << total << endl;
+
+    return 0;
+}
