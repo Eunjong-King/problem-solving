ff61b4a59e8f292c94899b9019ec678538d02ee1
Eunjong Kim <eunjong147@pusan.ac.kr>
2023-09-13 19:02:47 +0900

    [Gold IV] Title: 도시 건설, Time: 188 ms, Memory: 8276 KB -BaekjoonHub

---
+++ 백준/Gold/21924. 도시 건설/도시 건설.cc
@@ -0,0 +1,69 @@
+#define FAST_IO std::ios::sync_with_stdio(false); std::cin.tie(nullptr);
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+typedef vector<int> vi;
+
+vi parent(100002);
+
+int find_parent(int a) {
+    if (a == parent[a]) {
+        return a;
+    }
+
+    return parent[a] = find_parent(parent[a]);
+}
+
+void union_parent(int a, int b) {
+    int aref = find_parent(a);
+    int bref = find_parent(b);
+
+    parent[aref] = bref;
+}
+
+int main() {
+    FAST_IO;
+
+    int N, M;
+    cin >> N >> M;
+
+    for (int i = 0; i <= N; ++i) {
+        parent[i] = i;
+    }
+
+    vector<pair<int, pair<int, int>>> edges(M);
+    int a, b, cost;
+    long long total_cost = 0;
+    for (int i = 0; i < M; ++i) {
+        cin >> a >> b >> cost;
+
+        total_cost += cost;
+        edges[i] = {cost, {a, b}};
+    }
+
+    sort(edges.begin(), edges.end());
+
+    long long result = 0;
+    int count = 0;
+    for (auto edge: edges) {
+        a = edge.second.first;
+        b = edge.second.second;
+
+        if (find_parent(a) != find_parent(b)) {
+            union_parent(a, b);
+            result += edge.first;
+
+            count++;
+        }
+    }
+
+    if (count == N - 1) {
+        cout << total_cost - result << '\n';
+    } else {
+        cout << -1 << '\n';
+    }
+    return 0;
+}
