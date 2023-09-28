1562377e71766c86ff0a154477c60a60c03a166a
Eunjong Kim <eunjong147@pusan.ac.kr>
2023-09-28 14:04:41 +0900

    [Silver IV] Title: 정수 제곱근, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	long long n;
+	long long q;
+	
+	cin >> n;
+	
+	q = sqrt(n);
+
+	if ( (q*q) < n) {
+		++q;
+	}
+
+	cout << q;
+
+	return 0;
+}
