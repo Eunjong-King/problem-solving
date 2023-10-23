1c212e8175aeefda4e26dbfd5e922c399681f891
Eunjong Kim <eunjong147@pusan.ac.kr>
2023-10-23 20:17:46 +0900

    [Bronze III] Title: 현대모비스와 함께하는 부품 관리, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#define FAST_IO std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector <pii> vpii;
+typedef vector <vector<int>> vvi;
+typedef vector <vb> vvb;
+
+int main() {
+    FAST_IO;
+
+    int T;
+    cin >> T;
+
+    for (int j = 0; j < T; j++) {
+        int N;
+        cin >> N;
+
+        int A, B;
+        cin >> A >> B;
+
+        for (int i = 0; i < N; ++i) {
+            int u, v;
+            cin >> u >> v;
+        }
+
+        cout << "Material Management " << j + 1 << endl;
+        cout << "Classification ---- End!" << endl;
+    }
+
+
+    return 0;
+}
