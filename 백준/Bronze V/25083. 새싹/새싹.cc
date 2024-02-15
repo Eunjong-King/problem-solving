08a4ac3d140a704044f8f05fd1e2b3612027b4b3
Eunjong Kim <eunjong147@pusan.ac.kr>
2024-02-15 19:51:34 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze V/25083. 새싹/새싹.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#define FAST_IO std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<pii> vpii;
+typedef vector<vector<int>> vvi;
+typedef vector<vb> vvb;
+
+int main() {
+    FAST_IO;
+
+    cout << "         ,r'\"7\n"
+            "r`-_   ,'  ,/\n"
+            " \\. \". L_r'\n"
+            "   `~\\/\n"
+            "      |\n"
+            "      |" << endl;
+    return 0;
+}
